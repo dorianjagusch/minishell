12d2f9d989892af9d4b998251efd3cfe537933bb
Azzar Sarikhani Aghmiouni <asarikha@c2r4p1.hive.fi>
2023-04-05 13:52:09 +0300

    readmap added in main

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,7 +6,7 @@
 /*   By: asarikha <asarikha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/22 11:11:43 by asarikha          #+#    #+#             */
-/*   Updated: 2023/04/04 10:38:35 by asarikha         ###   ########.fr       */
+/*   Updated: 2023/04/04 12:14:32 by asarikha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,3 +15,4 @@
 # include <readline/readline.h>
 # include <readline/history.h>
 
+void		rl_replace_line(const char *text, int clear_undo);
--- src/main.c
+++ src/main.c
@@ -6,16 +6,29 @@
 /*   By: asarikha <asarikha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/22 11:11:43 by asarikha          #+#    #+#             */
-/*   Updated: 2023/04/04 10:35:48 by asarikha         ###   ########.fr       */
+/*   Updated: 2023/04/05 13:51:05 by asarikha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int	main(int argc, char **argv)
+int	main(int argc, char **argv, char **envp)
 {
 	char *line;
-	line = readline("\033[0;32DJASshell>\033[0m ");
-	printf("%s",line);
+
+	printf("%s",envp[0]);
+	if (argc < 1)
+		return (-1);
+	//copy envp
+	//add a level to shell
+	while(1)
+	{
+		//handle signals
+		line = readline("\e[34m""MiniShell$>""\x1b[m");
+		//syntax check, tokenize, parse
+		//expand, execute->if cmd is a buildtin execute builtin
+		//handle history
+		printf("%s\n",line);
+	}
 	return (0);
 }
