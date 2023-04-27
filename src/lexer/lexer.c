cb5c651dd3b212385503b169110fe1c6751532e8
Azzar Sarikhani Aghmiouni <asarikha@c2r5p5.hive.fi>
2023-04-27 16:50:49 +0300

    expander added

--- src/lexer/lexer.c
+++ src/lexer/lexer.c
@@ -6,11 +6,66 @@
 /*   By: asarikha <asarikha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/22 11:11:43 by asarikha          #+#    #+#             */
-/*   Updated: 2023/04/27 12:30:58 by asarikha         ###   ########.fr       */
+/*   Updated: 2023/04/27 16:47:11 by asarikha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+char	*expand_content(t_token **token, int start, t_env **env)
+{
+	int		end;
+	int		i;
+	char	*str;
+	char	*new;
+
+	end = start;
+	i = 0;
+	while (ft_isalnum(token->content[end]) && token->content[end])
+	{
+		str[i] = token->content[end];
+		end++;
+	}
+	str = find_env(env, str); //return null if it is not found in env
+	if (str)
+	{
+		i = ft_strlen(token->content) + 1;
+		new = ft_calloc(i * 1);
+		if (!new)
+			return (NULL);
+		new = replace_content(token->content, start, &new, str);
+		free(token->content);
+		token->content = new;
+	}
+	return (content);
+}
+
+int	expander(t_token **tokens, t_env **env)
+{
+	t_token	*temp;
+	int		i;
+
+	temp = *tokens;
+	while (tmp != NULL)
+	{
+		i = 0;
+		while (tmp->content[i])
+		{
+			if (tmp->content[i] == '$')
+			{
+				if (tmp->content[0] != '\'')
+				{
+					tmp->content = expand_content(&tmp, i, env);
+					if (tmp->content == NULL)
+						return (EXIT_FAILURE);
+				}
+			}
+			i++;
+		}
+		tmp = tmp->next;
+	}
+	return (EXIT_SUCCESS);
+}
 
 int	init_lexer(char *line, t_token	**tokens)
 {
@@ -38,5 +93,12 @@ int	init_lexer(char *line, t_token	**tokens)
 		if (indicator > i)
 			return (EXIT_FAILURE);
 	}
+	return (EXIT_SUCCESS);
+}
+
+int	retokenizer(t_token **tokens, t_env **env)
+{
+	expander(tokens, envp);
+	//get rid of extra quotes
 	return (EXIT_SUCCESS);
 }
--- src/lexer/token_utils.c
+++ src/lexer/token_utils.c
@@ -1,18 +1,39 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   token.c                                            :+:      :+:    :+:   */
+/*   token_utils.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: asarikha <asarikha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/22 11:11:43 by asarikha          #+#    #+#             */
-/*   Updated: 2023/04/27 12:28:51 by asarikha         ###   ########.fr       */
+/*   Updated: 2023/04/27 16:47:24 by asarikha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
 //strings include srounding single or double quotos
+void	replace_content(char *conent, int start, char **new, char *str)
+{
+	int		i;
+	int		end;
+	int		j;
+	int		len;
+
+	i = -1;
+	j = -1;
+	end = ft_strlen(str);
+	len = -1;
+	while (++i < start)
+		new[i] = content[++len];
+	while (++i < end)
+		new[i] = str[++j];
+	end = ft_strlen(content);
+	while (++i < end)
+		new[i] = content[++len];
+	new[i] = 0;
+}
+
 int	get_string(t_token **tokens, char *line, int quote)
 {
 	char	*str;
