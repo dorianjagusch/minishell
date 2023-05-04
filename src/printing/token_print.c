01a0673995ce62186f82b0708e51437e5221f5ea
djagusch <djagusch@student.hive.figit config --global user.name Dorian>
2023-05-04 17:14:42 +0300

    print function for token list added

--- src/printing/token_print.c
+++ src/printing/token_print.c
@@ -6,7 +6,7 @@
 /*   By: djagusch <djagusch@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/04 16:02:34 by djagusch          #+#    #+#             */
-/*   Updated: 2023/05/04 16:57:44 by djagusch         ###   ########.fr       */
+/*   Updated: 2023/05/04 17:13:53 by djagusch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,25 +14,25 @@
 
 void	print_token(t_token *token)
 {
-	t_token	tmp;
-
 	while (token)
 	{
 		if (token->token_type == STRING)
 			ft_printf("string: %s\n", token->content);
-		if (token->token_type == COMMAND)
+		else if (token->token_type == COMMAND)
 			ft_printf("cmd: %s\n", token->content);
-		if (token->token_type == GREATER_GREATER)
+		else if (token->token_type == GREATER_GREATER)
 			ft_printf(">>: %d\n", token->content);
-		if (token->token_type == GREATER_THAN)
+		else if (token->token_type == GREATER_THAN)
 			ft_printf("> %d\n", token->content);
-		if (token->token_type == LESS_LESS)
+		else if (token->token_type == LESS_LESS)
 			ft_printf("param: %d\n", token->content);
-		if (token->token_type == LESS_THAN)
+		else if (token->token_type == LESS_THAN)
 			ft_printf("param: %d\n", token->content);
-		if (token->token_type == PIPE)
+		else if (token->token_type == PIPE)
 			ft_printf("%s\n", token->content);
-		if (token->token_type == PIPE)
+		else if (token->token_type == SPACE)
 			ft_printf("SPACE: %s\n", token->content);
+		printf("quote: %d\n", token->isquote);
+		token = token->next;
 	}
 }
