34e684c4c1d45075665b021a94dc6c9c6a285d5b
djagusch <djagusch@student.hive.figit config --global user.name Dorian>
2023-08-31 13:56:03 +0300

    Fixed redirection issue #02

--- src/redir_exec/redirect.c
+++ src/redir_exec/redirect.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   redirect.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: asarikha <asarikha@student.hive.fi>        +#+  +:+       +#+        */
+/*   By: djagusch <djagusch@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/25 15:47:58 by djagusch          #+#    #+#             */
-/*   Updated: 2023/08/30 16:43:02 by asarikha         ###   ########.fr       */
+/*   Updated: 2023/08/31 13:53:38 by djagusch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,8 @@
 
 static void	enter_file_fds(t_command *command, int **pipes, int i)
 {
+	static int	previous_pipe;
+
 	if (command->infile && (pipes[i][0] != 0 || i == 0))
 	{
 		if (i != 0)
@@ -22,7 +24,7 @@ static void	enter_file_fds(t_command *command, int **pipes, int i)
 		if (command->fds[0] < 0)
 			command->success = -1;
 		pipes[i][0] = command->fds[0];
-		if (i != 0)
+		if (i != 0 && previous_pipe)
 			close(pipes[i][1]);
 	}
 	if (command->outfile && (pipes[i + 1][1] != 0 || i == g_info.n_cmd - 1))
@@ -32,9 +34,13 @@ static void	enter_file_fds(t_command *command, int **pipes, int i)
 		if (command->fds[1] < 0)
 			command->success = -1;
 		pipes[i + 1][1] = command->fds[1];
+		previous_pipe = 0;
 	}
 	else if (pipes[i + 1][1] == 0)
+	{
 		pipes[i + 1][1] = STDOUT_FILENO;
+		previous_pipe = 1;
+	}
 }
 
 static void	set_pipes(int **pipes, int n_cmd)
